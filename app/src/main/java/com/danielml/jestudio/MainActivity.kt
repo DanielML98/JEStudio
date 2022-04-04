342387227e122641aa9bd984ba31085ebf8b40f1
Daniel Martinez <danielmn685@gmail.com>
2022-04-03 20:55:11 -0500

    Initial commit

---
+++ app/src/main/java/com/danielml/jestudio/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.danielml.jestudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.danielml.jestudio.databinding.ActivityMainBinding
+import com.danielml.jestudio.fragments.BookFragment
+import com.danielml.jestudio.fragments.PaymentsFragment
+import com.danielml.jestudio.fragments.ProfileFragment
+
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+  private val profileFragment = ProfileFragment()
+  private val paymentsFragment = PaymentsFragment()
+  private val bookFragment = BookFragment()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    binding.bottomNavigation.setOnItemSelectedListener {
+      when(it.itemId) {
+        R.id.ic_book -> replaceFragment(bookFragment)
+        R.id.ic_payments -> replaceFragment(paymentsFragment)
+        R.id.ic_profile -> replaceFragment(profileFragment)
+      }
+      true
+    }
+  }
+
+  private fun replaceFragment(fragment: Fragment) {
+    if(fragment != null) {
+      val transaction = supportFragmentManager.beginTransaction()
+      transaction.replace(binding.fragmentContainer.id, fragment)
+      transaction.commit()
+    }
+  }
+}
